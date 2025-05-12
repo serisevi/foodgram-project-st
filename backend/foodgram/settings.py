c9647e4ee8e4da0a660d5e21384ab972e7596b75
Минаев Александр Викторович <fonvisinoff@yandex.ru>
2025-05-12 21:32:14 +0300

    Fixed static files for admin panel

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -117,7 +117,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/static/'
+STATIC_ROOT = '/app/static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
