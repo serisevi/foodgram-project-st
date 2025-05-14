ae36c6753ec8932d84eda39a38d55a45c5514308
Минаев Александр Викторович <fonvisinoff@yandex.ru>
2025-05-14 04:13:47 +0300

    fixed docker-push file

--- docker-push.sh
+++ docker-push.sh
@@ -15,14 +15,14 @@ docker-compose build
 
 # Тегирование образов для Docker Hub
 echo "Тегирование образов..."
-docker tag foodgram-st_backend serisevi/foodgram-backend:$VERSION
-docker tag foodgram-st_frontend serisevi/foodgram-frontend:$VERSION
-docker tag foodgram-st_gateway serisevi/foodgram-nginx:$VERSION
+docker tag "foodgram-st-backend" "serisevi/foodgram-backend:${VERSION}"
+docker tag "foodgram-st-frontend" "serisevi/foodgram-frontend:${VERSION}"
+docker tag "foodgram-st-gateway" "serisevi/foodgram-nginx:${VERSION}"
 
 # Отправка образов в Docker Hub
 echo "Отправка образов в Docker Hub..."
-docker push serisevi/foodgram-backend:$VERSION
-docker push serisevi/foodgram-frontend:$VERSION
-docker push serisevi/foodgram-nginx:$VERSION
+docker push "serisevi/foodgram-backend:${VERSION}"
+docker push "serisevi/foodgram-frontend:${VERSION}"
+docker push "serisevi/foodgram-nginx:${VERSION}"
 
-echo "Готово! Образы отправлены в Docker Hub как serisevi/foodgram-*:$VERSION" 
+echo "Готово! Образы отправлены в Docker Hub как serisevi/foodgram-*:${VERSION}" 
